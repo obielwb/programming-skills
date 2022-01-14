3a5917cf5026bc97e1a1db149a15598993da1882
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-01-13 22:49:14 -0300

    (OBI): add problem 'Idade da Dona Monica'

---
+++ obi/idadeDonaMonica.cpp
@@ -0,0 +1,19 @@
+/* OBI - 2019 - Fase 1 */
+
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+
+using namespace std;
+
+int main () {
+	int m, a, b;
+	
+	scanf("%d %d %d", &m, &a, &b);
+
+	int c = m - (a + b);
+	
+	int res = max(a, max(b, c));
+
+	printf("%d\n", res);	
+}
